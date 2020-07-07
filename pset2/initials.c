8ec2f979dea31c386c0fd2464904ae0101a2acfc
sudiyoleh <48716644+sudiyoleh@users.noreply.github.com>
2020-07-07 09:07:27 +0300

    Create initials.c

---
+++ pset2/initials.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+
+    string s = get_string();
+    if (s != NULL)
+    {
+        printf("%c", toupper(s[0]));
+        for (int i = 0, n = strlen(s); i < n; i++)
+        {
+            if (s[i] == ' ')
+            {
+                printf("%c", toupper(s[i+1]));
+            }
+        }
+        printf("\n");
+    }
+}
